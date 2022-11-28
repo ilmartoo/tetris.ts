d68affb3873009ee6cd0fbbd6c4a9fcd17424173
ilmartoo <martinszga@gmail.com>
2022-11-28 22:36:44 +0100

    Format fix

--- src/app/ts/Form.ts
+++ src/app/ts/Form.ts
@@ -1,4 +1,4 @@
-import {Color, Rotation, Vector} from './constants';
+import { Color, Rotation, Vector } from './constants';
 
 export class Form {
 	private readonly _size: Vector;
@@ -18,9 +18,12 @@ export class Form {
 	get color() { return this._color; }
 
 	rotate(direction: Rotation): Form {
-		const locations: Vector[] = [...this._locations];
-		const newLocations = direction === Rotation.left ? locations.map(l => ({x: this._size.y - l.y - 1, y: l.x} as Vector)) : locations.map(l => ({x: l.y, y: this._size.x - l.x - 1} as Vector));
-		return new Form({x: this._size.y, y: this._size.x} as Vector, newLocations, this.color);
+		const locations: Vector[] = [ ... this._locations ];
+		const newLocations = direction === Rotation.left ? locations.map(l => ({
+			x: this._size.y - l.y - 1,
+			y: l.x
+		} as Vector)) : locations.map(l => ({ x: l.y, y: this._size.x - l.x - 1 } as Vector));
+		return new Form({ x: this._size.y, y: this._size.x } as Vector, newLocations, this.color);
 	}
 }
 
@@ -32,11 +35,11 @@ const POS: Vector[][] = [
 ];
 
 export const FORMS = [
-	/* Line */ new Form({ x: 4, y: 1 }, [ POS[0][0], POS[1][0], POS[2][0], POS[3][0] ], Color.skyblue),
-	/* Cube */ new Form({ x: 2, y: 2 }, [ POS[0][0], POS[0][1], POS[1][0], POS[1][1] ], Color.yellow),
-	/* LR */ new Form({ x: 3, y: 2 }, [ POS[0][0], POS[1][0], POS[2][0], POS[2][1] ], Color.orange),
-	/* LL */ new Form({ x: 3, y: 2 }, [ POS[0][1], POS[1][1], POS[2][1], POS[2][0] ], Color.blue),
-	/* ZR */ new Form({ x: 2, y: 3 }, [ POS[0][0], POS[0][1], POS[1][1], POS[1][2] ], Color.red),
-	/* ZL */ new Form({ x: 2, y: 3 }, [ POS[0][1], POS[0][2], POS[1][1], POS[1][0] ], Color.green),
+	/* I */ new Form({ x: 4, y: 1 }, [ POS[0][0], POS[1][0], POS[2][0], POS[3][0] ], Color.skyblue),
+	/* O */ new Form({ x: 2, y: 2 }, [ POS[0][0], POS[0][1], POS[1][0], POS[1][1] ], Color.yellow),
+	/* L */ new Form({ x: 3, y: 2 }, [ POS[0][0], POS[1][0], POS[2][0], POS[2][1] ], Color.orange),
+	/* J */ new Form({ x: 3, y: 2 }, [ POS[0][1], POS[1][1], POS[2][1], POS[2][0] ], Color.blue),
+	/* Z */ new Form({ x: 2, y: 3 }, [ POS[0][0], POS[0][1], POS[1][1], POS[1][2] ], Color.red),
+	/* S */ new Form({ x: 2, y: 3 }, [ POS[0][1], POS[0][2], POS[1][1], POS[1][0] ], Color.green),
 	/* T */ new Form({ x: 2, y: 3 }, [ POS[0][1], POS[1][0], POS[1][1], POS[1][2] ], Color.purple)
 ];
